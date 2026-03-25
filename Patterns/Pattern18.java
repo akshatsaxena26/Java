e86ba2dcf914f33862c8f8629edbda287638130c
akshatsaxena26 <akshatsaxena023@gmail.com>
2026-03-25 11:25:52 +0530

    Refactor Pattern18 class to improve pattern generation logic and readability

--- Patterns/Pattern18.java
+++ Patterns/Pattern18.java
@@ -1,41 +1,44 @@
 package Patterns;
 
 public class Pattern18 {
-    public static void pattern18(int number) {
-        int spaces = number - 1;
-        int patternSize = 1;
 
-        for (int i = 1; i <= number; i++) {
+    public static void pattern18(int n) {
+        int patternSize = n;
+        int space = 0;
+        int rowMid = (n / 2) + 1;
+
+        for (int i = 1; i <= n; i++) {
             int num = 1;
-
-            for (int j = 1; j <= spaces; j++) {
-                System.out.print("  ");
-
-            }
             int colMid = (patternSize / 2) + 1;
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+            }
             for (int k = 1; k <= patternSize; k++) {
                 if (k < colMid) {
                     System.out.print(num++ + " ");
-
                 } else {
                     System.out.print(num-- + " ");
                 }
+            }
+            if (i < rowMid) {
+                patternSize -= 2;
+                space += 1;
+
+            } else {
+                patternSize += 2;
+                space -= 1;
 
             }
-            patternSize += 2;
-            spaces -= 1;
 
-            System.out.println("");
+            System.out.println(" ");
+
         }
 
     }
 
-    // This pattern is not completed
-
     public static void main(String[] args) {
-
-        int number = 5;
-        pattern18(number);
+        int n = 3;
+        pattern18(n);
 
     }
 }
